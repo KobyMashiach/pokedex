aa04a1e500e490d3f523832141162531a0a0b668
Koby Mashiach <koby9779@gmail.com>
2023-04-02 08:24:26 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/compile_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.compile_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:compile_project/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 153, 3, 3)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
